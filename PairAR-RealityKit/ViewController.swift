39f13785f965bfdd2e86fa7deae3f80dc81351d1
sun-23 <twinsamson@gmail.com>
2020-04-02 14:07:05 +0700

    test

--- PairAR-RealityKit/ViewController.swift
+++ PairAR-RealityKit/ViewController.swift
@@ -15,11 +15,34 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // to use AnchorEntity(plane: ,minimunBounds: ) please select device to iphone real device, or generic device
+        let anchor = AnchorEntity(plane: .horizontal, minimumBounds: [0.2, 0.2]) // 20 * 20 cm
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+        arView.scene.addAnchor(anchor)
         
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        var cards: [Entity] = []
+        for _ in 1...4 {
+            //create box
+            let box = MeshResource.generateBox(width: 0.04, height: 0.002, depth: 0.04)
+            //create material
+            let metalMaterial = SimpleMaterial(color: .gray, isMetallic: true)
+            
+            let model = ModelEntity(mesh: box, materials: [metalMaterial])
+            //generateCollisionShapes to can touch model
+            model.generateCollisionShapes(recursive: true)
+            
+            //add modelEntity to cards array
+            cards.append(model)
+        }
+        
+        for (index, card) in cards.enumerated() {
+            let x = Float(index % 2)
+            let z = Float(index % 2)
+            
+            card.position = [x * 0.1, 0, z * 0.1]
+            // add card
+            anchor.addChild(card)
+        }
+        
     }
 }
